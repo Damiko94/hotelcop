1a2c8897713dc5c8297a1b507822ee2f5564c56f
MrNakata <man2t1@gmail.com>
2020-01-31 07:58:45 +0100

    <Add>GetID+regExp email

--- script/js/script.js
+++ script/js/script.js
@@ -9,6 +9,21 @@ function printPage() {
   window.print();
 }
 
+// PAGE CONTACT
+var iNom = document.getElementById("ct-nom");
+var iPrenom = document.getElementById("ct-prenom");
+var iEmail1 = document.getElementById("ct-email");
+var iEmail2 = document.getElementById("ct-email2");
+
+var regEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+//source: regexr.com/2rhq7
+
+iEmail2.addEventListener("input", function(){
+  if (regEx.test(iEmail2.value)){
+    
+  }
+});
+
 
 /** ****************** **
  *
