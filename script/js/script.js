fb6853e84a40b45a73902b4b386a44e3e5d04377
MrNakata <man2t1@gmail.com>
2020-01-31 23:54:24 +0100

    <Fixed>Double branch JS-check-form

--- script/js/script.js
+++ script/js/script.js
@@ -9,21 +9,63 @@ function printPage() {
   window.print();
 }
 
-// PAGE CONTACT
-var iNom = document.getElementById("ct-nom");
-var iPrenom = document.getElementById("ct-prenom");
+/** ****************** **/
+//     PAGE CONTACT
+/** ****************** **/
+
 var iEmail1 = document.getElementById("ct-email");
 var iEmail2 = document.getElementById("ct-email2");
+// var iEmail1Help = document.getElementById("emailHelp");
+var iEmail2Help = document.getElementById("email2Help");
 
-var regEx = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+/** ****************** **/
+/**   SAISIE EMAIL 1   **/
+/** ****************** **/
 //source: regexr.com/2rhq7
+var regEx = /[a-z0-9]+(?:\.[a-z0-9]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
 
-iEmail2.addEventListener("input", function(){
-  if (regEx.test(iEmail2.value)){
-    
+//Vérifie que le mail saisi a le format d'un email
+iEmail1.addEventListener("input", function(){
+  var vEmail1 = iEmail1.value;
+  if (regEx.test(vEmail1)) {
+    // console.log("Email valide!");
+    setUi(iEmail1, false);
+    // Il n'est pas pertinent d'afficher que le mail saisi est valide.
+  }else{
+    // console.log("Email NON valide!");    
+    setUi(iEmail1, true);
   }
 });
 
+/** ****************** **/
+/** CONFIRMATION EMAIL **/
+/** ****************** **/
+iEmail2.addEventListener("input", function(){compareInputs(iEmail1, iEmail2);});
+
+function compareInputs(input1, input2){
+  var val1 = input1.value;
+  var val2 = input2.value;
+  // Affiche ok uniquement quand le mail est entièrement écrit et qu'ils correspondent
+  if (val1.length === val2.length && val1.substr(0,val2.length) === val2.substr(0,val2.length)) {
+    // console.log("Identiques!");
+    setUi(input2, false);
+    iEmail2Help.innerHTML="Les emails correspondent!";
+  }else{
+    // console.log("Pas Identiques!");
+    setUi(input2, true);
+    iEmail2Help.innerHTML="";
+  }
+}
+
+// Affiche pour l'utilisateur qu'emails correspondent
+function setUi(idInput, isInvalid){
+  var attr = "form-control";
+  var valid = "";
+  if (isInvalid){
+      valid = "in";
+  }
+  idInput.setAttribute("class", attr + " is-"+valid+"valid");  
+}
 
 /** ****************** **
  *
