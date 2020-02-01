b181b25c64899a7473ee56f9f9589c85edcb9204
MrNakata <man2t1@gmail.com>
2020-02-01 15:51:25 +0100

    <Add>Commentaires

--- script/js/script.js
+++ script/js/script.js
@@ -34,23 +34,48 @@ var iEmail2Help = document.getElementById("email2Help");
 var iMsgHelp = document.getElementById("msgHelp");
 var iCaptchaHelp = document.getElementById("captchaHelp");
 
-iNom.addEventListener("blur", function(){checkSaisie("ct-nom", iNom)});
-iPrenom.addEventListener("blur", function(){checkSaisie("ct-prenom", iPrenom)});
-iEntreprise.addEventListener("blur", function(){checkSaisie("ct-entreprise", iEntreprise)});
-iObjet.addEventListener("blur", function(){checkSaisie("ct-objet", iObjet)});
-iMsg.addEventListener("blur", function(){checkSaisie("ct-msg", iMsg)});
+/** ****************** **/
+/**  CHECK DES INPUTS  **/
+/** ****************** **/
+/* *
+ * Si user clique dans input et sort sans rien saisir
+ * affiche erreur
+ * */
+iNom.addEventListener("blur", function(){
+  if(iNom.value==""){
+    setUi(iNom, true);
+  }else{  
+   //Vérif que saisie correspond au RegExp   
+    checkSaisie("ct-nom", iNom)
+  }
+});
+iPrenom.addEventListener("blur", function(){if(iPrenom.value==""){setUi(iPrenom, true);}else{checkSaisie("ct-prenom", iPrenom)}});
+iEntreprise.addEventListener("blur", function(){if(iEntreprise.value==""){setUi(iEntreprise, true);}else{checkSaisie("ct-entreprise", iEntreprise)}});
+iEmail1.addEventListener("blur", function(){if(iEmail1.value==""){setUi(iEmail1, true);}else{checkSaisie("ct-email", iEmail1)}});
+iEmail2.addEventListener("blur", function(){if(iEmail2.value==""){setUi(iEmail2, true);}else{checkSaisie("ct-email2", iEmail2)}});
+iObjet.addEventListener("blur", function(){if(iObjet.value==""){setUi(iObjet, true);}else{checkSaisie("ct-objet", iObjet)}});
+iMsg.addEventListener("blur", function(){if(iMsg.value==""){setUi(iMsg, true);}else{checkSaisie("ct-msg", iMsg)}});
+
+/* *
+ * Vérif la saisie à chaque frappe du clavier et
+ * MAJ affichage du nombre restant de caractères
+ * */
 iMsg.addEventListener("input", function(){
   var vMsg = iMsg.value;  
   checkSaisie("ct-msg", iMsg);
   iMsgHelp.innerHTML = "Il vous reste " + (2000 - vMsg.length) + " caractères";
 });
 
+/* *
+ * Function recup name de l'input et var de l'input
+ * Test la saisie selon les RegExp
+ * Affiche bonne ou mauvaise saisie
+ * */
 function checkSaisie(nInput, iInput){
   //nInput : input name
   var regEx;
   var val = iInput.value;
   var minChar;
-  var attr = "form-control";
   switch (nInput) {
       case "ct-nom":
       case "ct-prenom":
@@ -71,10 +96,10 @@ function checkSaisie(nInput, iInput){
   if (val.length >= minChar) {
       if (regEx.test(val)) {
           // console.log(nInput + " correct!");
-          setUi(iInput, attr, false);
+          setUi(iInput, false);
       }else{
           // console.log(nInput + " Incorrect");
-          setUi(iInput, attr, true);
+          setUi(iInput, true);
       }
   }
   // console.log(val + " | " + regEx.test(val));
@@ -89,14 +114,13 @@ iEmail1.addEventListener("input", function(){
   //source: regexr.com/2rhq7
   var regEx = /[a-z0-9]+(?:\.[a-z0-9]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
   var vEmail1 = iEmail1.value;
-  var attr = "form-control";
   if (regEx.test(vEmail1)) {
     // console.log("Email valide!");
-    setUi(iEmail1, attr, false);
+    setUi(iEmail1, false);
     // Il n'est pas pertinent d'afficher que le mail saisi est valide.
   }else{
     // console.log("Email NON valide!");    
-    setUi(iEmail1, attr, true);
+    setUi(iEmail1, true);
   }
 });
 
@@ -108,21 +132,21 @@ iEmail2.addEventListener("input", function(){compareInputs(iEmail1, iEmail2);});
 function compareInputs(input1, input2){
   var val1 = input1.value;
   var val2 = input2.value;
-  var attr = "form-control";
   // Affiche ok uniquement quand le mail est entièrement écrit et qu'ils correspondent
   if (val1.length === val2.length && val1.substr(0,val2.length) === val2.substr(0,val2.length)) {
     // console.log("Identiques!");
-    setUi(input2, attr, false);
+    setUi(input2, false);
     iEmail2Help.innerHTML="Les emails correspondent!";
   }else{
     // console.log("Pas Identiques!");
-    setUi(input2, attr, true);
+    setUi(input2, true);
     iEmail2Help.innerHTML="";
   }
 }
 
 // Affiche pour l'utilisateur qu'emails correspondent
-function setUi(idInput, attr, isInvalid){  
+function setUi(idInput, isInvalid){  
+  var attr = "form-control";
   var valid = "";
   if (isInvalid){
       valid = "in";
@@ -160,13 +184,12 @@ function checkEmptyInputs() {
 /** ****************** **/
 //Listener input sur saisie du captcha
 iCaptcha.addEventListener("input", function(){
-  var attr = "form-control";
   var msg;
   if(checkCaptcha()){
-    setUi(iCaptcha, attr, false);
+    setUi(iCaptcha, false);
     iCaptchaHelp.innerHTML = "";
   }else{
-    setUi(iCaptcha, attr, true);
+    setUi(iCaptcha, true);
     msg = "Attention, le nombre saisi n'est pas le bon.";
     iCaptchaHelp.innerHTML = msg;
   }
@@ -206,7 +229,7 @@ function randCaptcha(){
 
 //Return un entier entre 0 et max passé en argument
 function alea_nb(max){
-	return Math.floor(Math.random() * max + 1);
+	return Math.floor(Math.random() * max);
 }
 /** ****************** **/
 /**   MESSAGE ENVOYE   **/
